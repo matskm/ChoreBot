10ae781f59de81c489b35854c92119b0e2704823
Mat Martin <matskm@gmail.com>
2016-11-03 22:55:58 +0000

    Example of playing mp3 file attached to button

--- app/src/main/java/com/matskm/chorebot/MainActivity.java
+++ app/src/main/java/com/matskm/chorebot/MainActivity.java
@@ -1,7 +1,9 @@
 package com.matskm.chorebot;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playSound1(View view){
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.voice_007);
+        mediaPlayer.start(); // no need to call prepare(); create() does that for you
     }
 
 
