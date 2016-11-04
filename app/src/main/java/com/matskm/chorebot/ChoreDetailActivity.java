c40edbd10517c1b94ac24171ae3fd1393267770d
Mat Martin <matskm@gmail.com>
2016-11-04 07:57:18 +0000

    Added ChoreDetail activity. Just the bare activity here.

---
+++ app/src/main/java/com/matskm/chorebot/ChoreDetailActivity.java
@@ -0,0 +1,13 @@
+package com.matskm.chorebot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoreDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chore_detail);
+    }
+}
