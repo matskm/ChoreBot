8cdcd56bbbfa8887dd9bdb794eac4ff5adadcf6f
Mat Martin <matskm@gmail.com>
2016-11-08 07:30:52 +0000

    Bare singleton implementation of ChoreRepository

--- chorebotlib/src/main/java/com/matskm/chorebotlib/ChoreRepository.java
+++ chorebotlib/src/main/java/com/matskm/chorebotlib/ChoreRepository.java
@@ -5,4 +5,19 @@ package com.matskm.chorebotlib;
  */
 
 public class ChoreRepository {
+
+    private static ChoreRepository instance = null;
+
+    protected ChoreRepository(){
+        // exists only to defeat instantiation
+    }
+
+    public static ChoreRepository getInstance(){
+        if(instance == null){
+            instance = new ChoreRepository();
+        }
+        return instance;
+
+    }
+    
 }
