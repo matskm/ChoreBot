c3d2f0d6e778cb988d36cd9d4dc8ca684f5541b4
Mat Martin <matskm@gmail.com>
2016-11-11 08:01:24 +0000

    Begin work on rehydrator

---
+++ chorebotlib/src/main/java/com/matskm/chorebotlib/IChoreRehyradtor.java
@@ -0,0 +1,9 @@
+package com.matskm.chorebotlib;
+
+/**
+ * Created by matskm on 11/11/16.
+ */
+
+public interface IChoreRehyradtor {
+    int rehyrdrate(ChoreRepository repo);
+}
---
+++ chorebotlib/src/test/java/com/matskm/chorebotlib/ChoreRehydratorTests.java
@@ -0,0 +1,19 @@
+package com.matskm.chorebotlib;
+
+/**
+ * Created by matskm on 11/11/16.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ChoreRehydratorTests {
+
+    @Test
+    public void rehydrate_GivenHardCodedRehydratorAndEmptyRepo_ResultsInCorrectRepo() {
+        // create a HardCodedRehydrator, and use it to rehydrate an empty repo... assert the size.
+        // Intefrace for rehydrate() method should be the number of elements its rehyrdated.
+    }
+
+
+}
